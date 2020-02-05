e61fd2e9d2e7c7f568168ff4f29986b2c83d35cd
Márton Török <torok.marton@greenformatics.hu>
2020-02-05 10:30:18 +0100

    Removed cra default

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Itt sportrehabilitácóval kapcsolatos oldal épül... Nézz vissza később! 😉</p>
       </header>
     </div>
   );
